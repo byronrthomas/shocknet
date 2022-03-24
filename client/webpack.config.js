bf5de0cbf27b0b32df5ca925ca4f53cf3a654717
Byron Thomas <byron.thomas@signal-ai.com>
2022-03-24 11:03:20 +0000

    Start of a non-react-y webpack project

---
+++ client/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: './src/index.js'
+  },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+   },
+   plugins: [new HtmlWebpackPlugin({
+    title: 'ByronT: TigerGraph GraphForAll Finance Challenge',
+    // Load a custom template (lodash by default)
+    template: 'src/template-index.html'
+  })]
+};

4be107d4482aacfdbd0d8f55b779bcdd5f7e5daf
Byron Thomas <byron.thomas@signal-ai.com>
2022-04-01 18:00:51 +0100

    Writing and checking condition info

--- tgfinance2022/shocks.py
+++ tgfinance2022/shocks.py
@@ -1,12 +1,13 @@
 import json
 import graph_db as db
+import urllib.parse
 
 def current_condition(conn):
     res = db.get_condition_info(conn)
     if res and len(res) > 0:
         if len(res) > 1:
             raise Exception(f"Bad condition info -- only expected one result not {len(res)}")
-        return res[0]['condition_description']
+        return res[0]['attributes']['condition_description']
 
 def assert_conditioned(conn):
     res = current_condition(conn)
@@ -26,8 +27,13 @@ def run_affected_countries_query(conn, cut_producer_ids):
         'affected_countries': res[0]['res'],
         'reachable_edges': res[1]['@@allEdges']}
 
+def format_percentage(fixed):
+    return f'{fixed / 10000.0}%'
+
 def condition_graph_fixed_point(conn, input_thresh, import_thresh, critical_ind_thresh):
-    params = f'input_pct_thresh={input_thresh}&import_pct_thresh={import_thresh}&national_output_thresh={critical_ind_thresh}'
+    # description = f'input_threshold = {format_percentage(input_thresh)} | import_threshold = {format_percentage(import_thresh)} | critical_industry_threshold = {format_percentage(critical_ind_thresh)}'
+    description = f'inputs_{input_thresh}_imports_{import_thresh}_critical_ind_{critical_ind_thresh}'
+    params = f'input_pct_thresh={input_thresh}&import_pct_thresh={import_thresh}&national_output_thresh={critical_ind_thresh}&description={description}'
     print('DEBUG - running with params string = ', params)
     res = conn.runInterpretedQuery(db.read_resource('resources/gsql_queries/condition_graph.gsql'), params)
     print(res)

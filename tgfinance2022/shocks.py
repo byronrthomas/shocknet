dfe58f49c00c35021e6dc620c0288e7d8c7d9a43
Byron Thomas <byron.thomas@signal-ai.com>
2022-04-09 20:59:51 +0100

    Fix typo in earlier change and remove some obsolete code

--- tgfinance2022/shocks.py
+++ tgfinance2022/shocks.py
@@ -121,7 +121,7 @@ def run_affected_countries_query(conn, supply_shocked_vertices):
         raise Exception("Cannot handle vertices that aren't producers!!")
     cut_params = [
         f'starting_nodes[{i}]={v["v_id"]}&starting_nodes[{i}].type=producer'
-        for i, v in enumerate(supply_shocked_vertices)]
+        for i, v in enumerate(producer_vertices)]
     cut_params = '&'.join(cut_params)
     params = f'{cut_params}&allowed_edge_types={db.CRITICAL_INDUSTRY_EDGE}&allowed_edge_types={db.TRADE_SHOCK_EDGE}&allowed_edge_types={db.PRODUCTION_SHOCK_EDGE}&allowed_vertex_types={db.COUNTRY_VERTEX}&allowed_vertex_types={db.PRODUCER_VERTEX}&final_vertex_types={db.COUNTRY_VERTEX}&report_links=TRUE'
     print('DEBUG - running with params string = ', params)
@@ -151,8 +151,6 @@ def set_condition(conn, **params):
     db.upsert_nodes(conn, db.CONDITION_VERTEX, [(1, {'condition_description': json.dumps(params)})])
 
 def condition_graph_fixed_point(conn, input_thresh, import_thresh, critical_ind_thresh):
-    # description = f'input_threshold = {format_percentage(input_thresh)} | import_threshold = {format_percentage(import_thresh)} | critical_industry_threshold = {format_percentage(critical_ind_thresh)}'
-    description = f'inputs_{input_thresh}_imports_{import_thresh}_critical_ind_{critical_ind_thresh}'
     params = f'input_pct_thresh={input_thresh}&import_pct_thresh={import_thresh}&national_output_thresh={critical_ind_thresh}'
     print('DEBUG - running with params string = ', params)
     res = conn.runInterpretedQuery(db.read_resource('resources/gsql_queries/condition_graph.gsql'), params)

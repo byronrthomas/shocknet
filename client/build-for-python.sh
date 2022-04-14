bfd3a6b53f75997e1a40b75d042e0acbc3f7ebd9
Byron Thomas <byron.thomas@signal-ai.com>
2022-04-14 18:13:20 +0100

    Move client build into client folder to pick correct version of node

--- client/build-for-python.sh
+++ client/build-for-python.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-cd client
 echo Attempting to run webpack
 npm run build
-cd ..
 echo Removing python static assets
-rm -rf tgfinance2022/static
-cp -R client/dist tgfinance2022/static
+rm -rf ../tgfinance2022/static
+cp -R dist ../tgfinance2022/static
 echo Done

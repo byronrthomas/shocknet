2c6cd4887335b31f3cc51c1d2e09cc9554aefa39
Byron Thomas <byron.thomas@signal-ai.com>
2022-04-15 15:46:26 +0100

    Move client build script back to root now we've figured out issue - Check in latest version of JS files (built for local, not docker)

--- build-client.sh
+++ build-client.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+cd client
+fnm use
 echo Attempting to run webpack
 npm run build
+cd ..
 echo Removing python static assets
-rm -rf ../shocknet/static
-cp -R dist ../shocknet/static
+rm -rf shocknet/static
+cp -R client/dist shocknet/static
 echo Done
